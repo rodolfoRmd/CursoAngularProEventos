ff9a8e0eed145fe412b98fb81db751b0d0e095b1
rodolfoRmd <rodolfo.rr10@gmail.com>
2022-03-25 21:21:55 -0300

    Layout Responsivo + Forms

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -22,7 +22,12 @@ import { NgxSpinnerModule } from "ngx-spinner";
 import { TituloComponent } from './shared/titulo/titulo.component';
 import { ContatosComponent } from './components/contatos/contatos.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
+import { PerfilComponent } from './components/user/perfil/perfil.component';
+import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
+import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
+import { UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +39,12 @@ import { PerfilComponent } from './components/perfil/perfil.component';
       TituloComponent,
       ContatosComponent,
       DashboardComponent,
-      PerfilComponent
+      PerfilComponent,
+      EventoDetalheComponent,
+      EventoListaComponent,
+      UserComponent,
+      LoginComponent,
+      RegistrationComponent
 
    ],
   imports: [
--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-titulo',
@@ -7,9 +8,16 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TituloComponent implements OnInit {
   @Input() nomeTitulo: string='';
-  constructor() { }
+  @Input() iconClass: string = 'fa fa-user';
+  @Input() subTitulo: string = 'Desde 2021';
+  @Input() botaoListar: boolean=false;
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  listar():void{
+    this.router.navigate([`/${this.nomeTitulo.toLocaleLowerCase()}/lista`])
   }
 
 }
